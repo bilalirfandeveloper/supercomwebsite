27440e828c0401d68c839184fc7ac6874837957f
bilalirfandeveloper <bilalirfandeveloper@gmail.com>
2024-12-02 23:56:03 +0500

    main js changes

--- js/main.js
+++ js/main.js
@@ -108,33 +108,7 @@ window.onload = function () {
     });
 
 
-    // Progress Bar
-    $('.pg-bar').waypoint(function () {
-        $('.progress .progress-bar').each(function () {
-            $(this).css("width", $(this).attr("aria-valuenow") + '%');
-        });
-    }, {offset: '80%'});
-
-
-    // Calender
-    $('#calender').datetimepicker({
-        inline: true,
-        format: 'L'
-    });
-
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        items: 1,
-        dots: true,
-        loop: true,
-        nav : false
-    });
-
-
-   
+    
 
 })(jQuery);
 
